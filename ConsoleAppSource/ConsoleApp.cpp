c143ebaf5a4cb5a8f8100cdd13eb2afc918acbcf
Trillaman <26063289+trillaman@users.noreply.github.com>
2023-06-22 16:19:29 +0200

    Added extra parameters to invoking new process in order to alert cmds with parameters

--- ConsoleAppSource/ConsoleApp.cpp
+++ ConsoleAppSource/ConsoleApp.cpp
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <vector>
 
 
 #define MAX_LENGTH 300
@@ -32,6 +33,37 @@ int invokeProcess(char* pathToExe) {
 
 }
 
+int invokeProcessWithDummyParameters(char* pathToExe, std::vector<std::string> list) {
+	for (int i = 0; i < list.size(); i++) {
+		std::cout << list[i];
+	}
+	WCHAR* execmd;
+	execmd = new WCHAR[MAX_LENGTH];
+	MultiByteToWideChar(CP_ACP, 0, pathToExe, -1, execmd, MAX_LENGTH);
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	ZeroMemory(&pi, sizeof(pi));
+
+	if (!CreateProcess(LPCWSTR(execmd), NULL, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
+		printf("CreateProcess failed (%d).\n", GetLastError());
+		return 1;
+	}
+	WaitForSingleObject(pi.hProcess, INFINITE);
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+
+	return 0;
+}
+
+char* initArrayAndReturnIt(char* array, int max_length) {
+	for (int i = 0; i < max_length; i++) {
+		array[i] = '\0';
+	}
+	return array;
+}
+
 int loadLibrary(char* argvDllPath) {
 	HINSTANCE hGetProcIDLL = NULL;
 
@@ -52,7 +84,7 @@ int loadLibrary(char* argvDllPath) {
 		printf("Error: %d", GetLastError());
 		return 1;
 	}
-	
+
 	return 0;
 }
 
@@ -72,46 +104,53 @@ int main(int argc, char** argv)
 		return EXIT_FAILURE;
 	}
 
-	for (int i = 0; i < argc; ++i) {
-		if (std::string(argv[1]) == "-fork") {
-			if (argc == 3) {
-				strcpy_s(processName, argv[2]);
-				printf("Forking process: %s\n", processName);
-				invokeProcess(processName);
-				break;
-			}
-			else {
-				printf("Not enough arguments\n");
-				return 1;
-			}
+	if (std::string(argv[1]) == "-fork") {
+		if (argc == 3) {
+			strcpy_s(processName, argv[2]);
+			printf("Forking process: %s\n", processName);
+			invokeProcess(processName);
 		}
-		else if (std::string(argv[1]) == "-load") {
-			if (argc == 3) {
-				if (strlen(argv[1]) > MAX_LENGTH) {
-					printf("Path argument is too long. Max length is %d", MAX_LENGTH);
-					return EXIT_FAILURE;
-				}
-				else {
-					printf("\nPath you entered for dll is %s\n\n", argv[2]);
-					strcpy_s(argvDllPath, sizeof argvDllPath, argv[2]);
-					loadLibrary(argvDllPath);
-					break;
-				}
+		else if (argc > 3) {
+			std::vector<std::string> list;
+			for (int i = 3; i < argc; i++) {
+				std::string tmp(argv[i]);
+				list.push_back(tmp); // add all arguments to the vector
 			}
-			else if (argc == 2) {
-				printf("Please enter the path for dll: ");
-				if (scanf_s("%s", argvDllPath, MAX_LENGTH) != 1) {
-					printf("Path should not be longer than %d", MAX_LENGTH);
-					return EXIT_FAILURE;
-				}
-				else {
-					loadLibrary(argvDllPath);
-					break;
-				}
-			}
+
+			strcpy_s(processName, argv[2]);
+			printf("Forking process: %s\n", processName);
+			invokeProcessWithDummyParameters(processName, list);
+		}
+		else {
+			printf("Not enough arguments\n");
+			return 1;
 		}
 	}
-		
+	else if (std::string(argv[1]) == "-load") {
+		if (argc == 3) {
+			if (strlen(argv[1]) > MAX_LENGTH) {
+				printf("Path argument is too long. Max length is %d", MAX_LENGTH);
+				return EXIT_FAILURE;
+			}
+			else {
+				printf("\nPath you entered for dll is %s\n\n", argv[2]);
+				strcpy_s(argvDllPath, sizeof argvDllPath, argv[2]);
+				loadLibrary(argvDllPath);
+			}
+		}
+		else if (argc == 2) {
+			printf("Please enter the path for dll: ");
+			if (scanf_s("%s", argvDllPath, MAX_LENGTH) != 1) {
+				printf("Path should not be longer than %d", MAX_LENGTH);
+				return EXIT_FAILURE;
+			}
+			else {
+				loadLibrary(argvDllPath);
 
+			}
+
+		}
+
+	}
 	return EXIT_SUCCESS;
 }
